15f9050fd8f1d319ca21ea9361bd5f5526bb9093
Andreas Noack <andreasnoackjensen@gmail.com>
2017-07-03 23:04:09 -0400

    Fix vector calculation in symmetric eigenvalue problem
    Add more high level convenience methods

--- src/eigenSelfAdjoint.jl
+++ src/eigenSelfAdjoint.jl
@@ -4,13 +4,57 @@ module EigenSelfAdjoint
 
     immutable SymmetricTridiagonalFactorization{T} <: Factorization{T}
         factors::Matrix{T}
-        scalarfactors::Vector{T}
+        τ::Vector{T}
         diagonals::SymTridiagonal
     end
 
+    Base.size(S::SymmetricTridiagonalFactorization, i::Integer) = size(S.factors, i)
+
+    immutable EigenQ{T} <: AbstractMatrix{T}
+        factors::Matrix{T}
+        τ::Vector{T}
+    end
+
+    getq(S::SymmetricTridiagonalFactorization) = EigenQ(S.factors, S.τ)
+
+    Base.size(Q::EigenQ) = (size(Q.factors, 1), size(Q.factors, 1))
+    function Base.size(Q::EigenQ, i::Integer)
+        if i < 1
+            throw(ArgumentError(""))
+        elseif i < 3
+            return size(Q.factors, 1)
+        else
+            return 1
+        end
+    end
+
+    function Base.LinAlg.A_mul_B!(Q::EigenQ, B::StridedVecOrMat)
+        m, n = size(B)
+        if size(Q, 1) != m
+            throw(DimensionMismatch(""))
+        end
+        for k in length(Q.τ):-1:1
+            for j in 1:size(B, 2)
+                b = view(B, :, j)
+                vb = B[k + 1, j]
+                for i in (k + 2):m
+                    vb += Q.factors[i, k]'B[i, j]
+                end
+                τkvb = Q.τ[k]'vb
+                B[k + 1, j] -= τkvb
+                for i in (k + 2):m
+                    B[i, j] -= Q.factors[i, k]*τkvb
+                end
+            end
+        end
+        return B
+    end
+
+    Base.LinAlg.full(Q::EigenQ) = A_mul_B!(Q, eye(eltype(Q), size(Q, 1), size(Q, 1)))
+
     function eigvals2x2(d1::Number, d2::Number, e::Number)
-        r1 = 0.5*(d1 + d2)
-        r2 = 0.5hypot(d1 - d2, 2*e)
+        r1 = (d1 + d2)/2
+        r2 = hypot(d1 - d2, 2*e)/2
         return r1 + r2, r1 - r2
     end
     function eigQL2x2!(d::StridedVector, e::StridedVector, j::Integer, vectors::Matrix)
@@ -29,14 +73,13 @@ module EigenSelfAdjoint
             c /= h
             s = inv(h)
         else
-            @show 
             c = one(c)
             s = zero(c)
         end
         if vectors != nothing
             for i = 1:size(vectors, 1)
-                v1 = vectors[i,j + 1]
-                v2 = vectors[i,j]
+                v1 = vectors[i, j + 1]
+                v2 = vectors[i, j]
                 vectors[i, j + 1] = c*v1 + s*v2
                 vectors[i, j]     = c*v2 - s*v1
             end
@@ -45,7 +88,7 @@ module EigenSelfAdjoint
         return c, s
     end
 
-    function eigvalsPWK!{T<:AbstractFloat}(S::SymTridiagonal{T}, tol = eps(T), debug::Bool=false)
+    function eigvalsPWK!{T<:Real}(S::SymTridiagonal{T}, tol = eps(T), debug::Bool=false)
         d = S.dv
         e = S.ev
         n = length(d)
@@ -93,7 +136,10 @@ module EigenSelfAdjoint
         sort!(d)
     end
 
-    function eigQL!{T<:AbstractFloat}(S::SymTridiagonal{T}, vectors::Matrix = zeros(T, 0, size(S, 1)), tol = eps(T), debug::Bool=false)
+    function eigQL!{T<:Real}(S::SymTridiagonal{T},
+                             vectors::Matrix = zeros(T, 0, size(S, 1)),
+                             tol = eps(T),
+                             debug::Bool=false)
         d = S.dv
         e = S.ev
         n = length(d)
@@ -143,7 +189,7 @@ module EigenSelfAdjoint
         return d[p], vectors[:,p]
     end
 
-    function eigQR!{T<:AbstractFloat}(S::SymTridiagonal{T}, vectors::Matrix = zeros(T, 0, size(A, 1)), tol = eps(T), debug::Bool=false)
+    function eigQR!{T<:Real}(S::SymTridiagonal{T}, vectors::Matrix = zeros(T, 0, size(A, 1)), tol = eps(T), debug::Bool=false)
         d = S.dv
         e = S.ev
         n = length(d)
@@ -187,7 +233,10 @@ module EigenSelfAdjoint
     end
 
     # Own implementation based on Parlett's book
-    function singleShiftQLPWK!(S::SymTridiagonal, shift::Number, istart::Integer = 1, iend::Integer = length(S.dv))
+    function singleShiftQLPWK!(S::SymTridiagonal,
+                               shift::Number,
+                               istart::Integer = 1,
+                               iend::Integer = length(S.dv))
         d = S.dv
         e = S.ev
         n = length(d)
@@ -197,18 +246,18 @@ module EigenSelfAdjoint
         s = zero(eltype(S))
         @inbounds for i = iend-1:-1:istart
             ei = e[i]
-            ζ = π + ei
+            ζ  = π + ei
             if i < iend-1
                 e[i+1] = s*ζ
             end
-            ci1 = ci
-            ci = π/ζ
-            s = ei/ζ
-            di = d[i]
-            γi1 = γi
-            γi = ci*(di - shift) - s*γi1
+            ci1    = ci
+            ci     = π/ζ
+            s      = ei/ζ
+            di     = d[i]
+            γi1    = γi
+            γi     = ci*(di - shift) - s*γi1
             d[i+1] = γi1 + di - γi
-            π = ci == 0 ? ei*ci1 : γi*γi/ci
+            π      = ci == 0 ? ei*ci1 : γi*γi/ci
         end
         e[istart] = s*π
         d[istart] = shift + γi
@@ -216,7 +265,11 @@ module EigenSelfAdjoint
     end
 
     # Own implementation based on Parlett's book
-    function singleShiftQL!(S::SymTridiagonal, shift::Number, istart::Integer = 1, iend::Integer = length(S.dv), vectors = zeros(eltype(S), 0, size(S, 1)))
+    function singleShiftQL!(S::SymTridiagonal,
+                            shift::Number,
+                            istart::Integer = 1,
+                            iend::Integer = length(S.dv),
+                            vectors = zeros(eltype(S), 0, size(S, 1)))
         d = S.dv
         e = S.ev
         n = length(d)
@@ -242,8 +295,8 @@ module EigenSelfAdjoint
             for k = 1:size(vectors, 1)
                 v1 = vectors[k, i + 1]
                 v2 = vectors[k, i]
-                vectors[k, i + 1]     = ci*v1 + si*v2
-                vectors[k, i] = ci*v2 - si*v1
+                vectors[k, i + 1] = ci*v1 + si*v2
+                vectors[k, i]     = ci*v2 - si*v1
             end
         end
         e[istart] = si*π
@@ -252,7 +305,11 @@ module EigenSelfAdjoint
     end
 
     # Own implementation based on Parlett's book
-    function singleShiftQR!(S::SymTridiagonal, shift::Number, istart::Integer = 1, iend::Integer = length(S.dv), vectors = zeros(eltype(S), 0, size(S, 1)))
+    function singleShiftQR!(S::SymTridiagonal,
+                            shift::Number,
+                            istart::Integer = 1,
+                            iend::Integer = length(S.dv),
+                            vectors = zeros(eltype(S), 0, size(S, 1)))
         d = S.dv
         e = S.ev
         n = length(d)
@@ -321,7 +378,7 @@ module EigenSelfAdjoint
         τ = zeros(T,n-1)
         u = Matrix{T}(n, 1)
         @inbounds begin
-        for k = 1:n-2+!(T<:Real)
+        for k = 1:(n - 2 + !(T<:Real))
             τk = LinAlg.reflector!(view(AS, k + 1:n, k))
             τ[k] = τk
 
@@ -370,21 +427,35 @@ module EigenSelfAdjoint
         SymmetricTridiagonalFactorization(AS,τ,SymTridiagonal(real(diag(AS)),real(diag(AS,-1))))
     end
 
-    eigvals!(A::SymmetricTridiagonalFactorization, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvalsPWK!(A.diagonals, tol, debug)
-    eigvals!(A::Hermitian, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvals!(symtri!(A), tol, debug)
-    eigvals!{T<:Real}(A::Symmetric{T}, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvals!(symtri!(A), tol, debug)
+    eigvals!(         A::SymmetricTridiagonalFactorization, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvalsPWK!(A.diagonals, tol, debug)
+    eigvals!{T<:Real}(A::LinAlg.RealHermSymComplexHerm{T},  tol = eps(real(float(one(eltype(A))))), debug = false) = eigvals!(symtri!(A), tol, debug)
+    eigvals!(         A::SymTridiagonal,                    tol = eps(real(float(one(eltype(A))))), debug = false) = eigvalsPWK!(A, tol, debug)
 
-    eigvals!(A::SymTridiagonal, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvalsPWK!(A, tol, debug)
-    eig!(A::SymTridiagonal, tol = eps(real(float(one(eltype(A))))), debug = false) = eigQL!(A, eye(eltype(A), size(A, 1)), tol, debug)
+    eig!(A::SymmetricTridiagonalFactorization, tol = eps(real(float(one(eltype(A))))), debug = false) = eigQL!(A.diagonals, full(getq(A)), tol, debug)
+    eig!(A::SymTridiagonal,                    tol = eps(real(float(one(eltype(A))))), debug = false) = eigQL!(A, eye(eltype(A), size(A, 1)), tol, debug)
+    eig!(A::LinAlg.RealHermSymComplexHerm,     tol = eps(real(float(one(eltype(A))))), debug = false) = eig!(symtri!(A), tol, debug)
+
     function eig2!(A::SymTridiagonal, tol = eps(real(float(one(eltype(A))))), debug = false)
         V = zeros(eltype(A), 2, size(A, 1))
         V[1] = 1
         V[end] = 1
         eigQL!(A, V, tol, debug)
     end
+    eig2!(A::LinAlg.RealHermSymComplexHerm,   tol = eps(real(float(one(eltype(A))))), debug = false) = eig2!(symtri!(A), tol, debug)
 
-    eigvals(A::SymTridiagonal, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvals!(copy(A), tol, debug)
-    eig(A::SymTridiagonal) = eig!(copy(A))
-    eig2(A::SymTridiagonal) = eig2!(copy(A))
+    eigvals(A::SymTridiagonal,                tol = eps(real(float(one(eltype(A))))), debug = false) = eigvals!(copy(A), tol, debug)
+    eigvals(A::LinAlg.RealHermSymComplexHerm, tol = eps(real(float(one(eltype(A))))), debug = false) = eigvals!(copy(A), tol, debug)
+
+    eig(A::SymTridiagonal               ,     tol = eps(real(float(one(eltype(A))))), debug = false) = eig!(copy(A)    , tol, debug)
+    eig(A::LinAlg.RealHermSymComplexHerm,     tol = eps(real(float(one(eltype(A))))), debug = false) = eig!(copy(A)    , tol, debug)
+
+    eig2(A::SymTridiagonal               ,    tol = eps(real(float(one(eltype(A))))), debug = false) = eig2!(copy(A)   , tol, debug)
+    eig2(A::LinAlg.RealHermSymComplexHerm,    tol = eps(real(float(one(eltype(A))))), debug = false) = eig2!(copy(A)   , tol, debug)
+
+    # Aux (should go somewhere else at some point)
+    function Base.LinAlg.givensAlgorithm(f::Real, g::Real)
+        h = hypot(f, g)
+        return f/h, g/h, h
+    end
 
 end
--- test/eigenselfadjoint.jl
+++ test/eigenselfadjoint.jl
@@ -9,9 +9,17 @@ using LinearAlgebra
 
 @testset "The selfadjoint eigen problem" begin
     n = 10
-    T = SymTridiagonal(randn(n), randn(n - 1))
-    vals, vecs  = LinearAlgebra.EigenSelfAdjoint.eig(T)
-    @test (vecs*Diagonal(vals))*vecs' ≈ full(T)
-    vals, vecs2 = LinearAlgebra.EigenSelfAdjoint.eig2(T)
-    @test vecs[[1,end],:] == vecs2
+    @testset "SymTridiagonal" begin
+        T = SymTridiagonal(randn(n), randn(n - 1))
+        vals, vecs  = LinearAlgebra.EigenSelfAdjoint.eig(T)
+        @test (vecs*Diagonal(vals))*vecs' ≈ full(T)
+        vals, vecs2 = LinearAlgebra.EigenSelfAdjoint.eig2(T)
+        @test vecs[[1,end],:] == vecs2
+    end
+
+    @testset "(full) Symmetric" begin
+        A = Symmetric(randn(n, n), :L)
+        vals, vecs = LinearAlgebra.EigenSelfAdjoint.eig(A)
+        @test vecs'*A*vecs ≈ diagm(vals)
+    end
 end
