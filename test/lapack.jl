2e2a0f803a339513d6f21988d1259540a080a451
Andreas Noack <andreasnoackjensen@gmail.com>
2018-01-17 11:27:55 +0100

    Check in the LAPACK test file

---
+++ test/lapack.jl
@@ -0,0 +1,72 @@
+using Base.Test
+using LinearAlgebra
+using LinearAlgebra.LAPACK2
+
+@testset "LAPACK wrappers" begin
+
+    n = 100
+
+    T = SymTridiagonal(randn(n), randn(n - 1))
+    vals, vecs = eig(T)
+    @testset "steqr" begin
+        _vals, _vecs = LAPACK2.steqr!('N', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+
+        _vals, _vecs = LAPACK2.steqr!('I', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+
+        _vals, _vecs = LAPACK2.steqr!('V', copy(T.dv), copy(T.ev), eye(n))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+    end
+
+    @testset "sterf" begin
+        _vals = LAPACK2.sterf!(copy(T.dv), copy(T.ev))
+        @test _vals ≈ vals
+    end
+
+    @testset "stedc" begin
+        _vals, _vecs = LAPACK2.stedc!('N', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+
+        _vals, _vecs = LAPACK2.stedc!('I', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+
+        _vals, _vecs = LAPACK2.stedc!('V', copy(T.dv), copy(T.ev), eye(n))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+    end
+
+    @testset "stemr" begin
+        _vals, _vecs, __ = LAPACK2.stemr!('N', 'A', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+
+        _vals, _vecs, __ = LAPACK2.stemr!('N', 'V', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+
+        _vals, _vecs, __ = LAPACK2.stemr!('N', 'I', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+
+        _vals, _vecs, __ = LAPACK2.stemr!('V', 'A', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+
+        _vals, _vecs, __ = LAPACK2.stemr!('V', 'V', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+
+        _vals, _vecs, __ = LAPACK2.stemr!('V', 'I', copy(T.dv), copy(T.ev))
+        @test vals ≈ _vals
+        @test abs.(_vecs'vecs) ≈ eye(n)
+    end
+
+    @testset "lahqr" begin
+        T = Array(Tridiagonal(fill(0.1, 99), fill(0.0, 100), fill(40.0, 99)))
+        _vals = sort(LinearAlgebra.LAPACK2.lahqr!(copy(T))[1])
+        @test _vals ≈ sort(real.(LinearAlgebra.EigenGeneral.eigvals!(copy(T))))
+        # LAPACK's multishift algorithm (the default) seems to be broken
+        @test !(_vals ≈ sort(eigvals(T)))
+    end
+end
